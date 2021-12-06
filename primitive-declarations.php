27627bab29b106232cfa105023b9954d83c908ca
Mathias LEROY <mathlry147@gmail.com>
2021-12-06 14:13:12 +0100

    php

---
+++ primitive-declarations.php
@@ -0,0 +1,10 @@
+<?php
+
+$firstStr = "" ;
+$firstEscapeStr = "_";
+$firstBool = true;
+$firstInteger = 2;
+$firstFloat = 2.1;
+$firstNull = NULL;
+$firstArray = array(2, null, 'Hi', 5.5, false);
+?>
