3b83ebd1eab5844d69b58d1004f79ee010828d2c
Mathias LEROY <mathlry147@gmail.com>
2021-12-07 13:33:29 +0100

    php

--- multiplication-table.php
+++ multiplication-table.php
@@ -1,10 +1,8 @@
 <?php
     $table = 9;
     $length = 10;
-    $i = 1;
-    
+    $i = 1;    
     while($i <= $length) {
      echo "$i * $table = ".$i * $table. "\n";
-     $i++;
-    }
+     $i++;}
 ?>
