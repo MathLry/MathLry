0db780304416f41ab8db5a678b1e570a507f49ae
Mathias LEROY <mathlry147@gmail.com>
2021-12-07 16:22:50 +0100

    ggf

--- what-month-is-it.php
+++ what-month-is-it.php
@@ -1,51 +1,51 @@
 <?php
     $month = 12;
     switch ($month) {
-        case '1':
+        case 1:
         echo "We are in the month of January";
         break;
 
-        case '2':
+        case 2:
         echo "We are in the month of February";
         break;
         
-        case '3':
+        case 3:
         echo "We are in the month of Mars";
         break;
         
-        case '4':
+        case 4:
             echo "We are in the month of April";
         break;
 
-        case '5':
+        case 5:
         echo "We are in the month of May";
         break;
         
-        case '6':
+        case 6:
         echo "We are in the month of June";
         break;
         
-        case '7':
+        case 7:
         echo "We are in the month of July";
         break;
         
-        case '8':
+        case 8:
         echo "We are in the month of August";
         break;
         
-        case '9':
+        case 9:
         echo "We are in the month of September";
         break;
         
-        case '10':
+        case 10:
         echo "We are in the month of October";
         break;
         
-        case '11':
+        case 11:
         echo "We are in the month of November";
         break;
         
-        case '12':
+        case 12:
         echo "We are in the month of December";
         break;
     }
