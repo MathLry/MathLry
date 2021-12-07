8c5a6372f0e81321f36e850bd3aefa5ae97635a6
Mathias LEROY <mathlry147@gmail.com>
2021-12-07 13:28:54 +0100

    php

--- be-polite.php
+++ be-polite.php
@@ -1,13 +1,12 @@
 <?php
-
-$hour = gettimeofday("h:i:a");
-if ((6 < $hour) and ($hour > 12)) {
-    echo "Hello! Have a nice day :)";
-} elseif ((12 <= $hour) and ($hour >= 18)) {
-    echo "Have a good afternoon!";
-} elseif ((18 <= $hour) and ($hour >= 21)) {
-    echo "Good evening! Hope you had a good day!";
-}else {
-    echo "Good night! See you tomorrow :)";
-}
+    $hour = gettimeofday("h:i:a");
+    if ((6 < $hour) and ($hour > 12)) {
+        echo "Hello! Have a nice day :)";
+    } elseif ((12 <= $hour) and ($hour >= 18)) {
+        echo "Have a good afternoon!";
+    } elseif ((18 <= $hour) and ($hour >= 21)) {
+        echo "Good evening! Hope you had a good day!";
+    }else {
+        echo "Good night! See you tomorrow :)";
+    }
 ?>
---
+++ multiplication-table.php
@@ -0,0 +1,10 @@
+<?php
+    $table = 9;
+    $length = 10;
+    $i = 1;
+    
+    while($i <= $length) {
+     echo "$i * $table = ".$i * $table. "\n";
+     $i++;
+    }
+?>
