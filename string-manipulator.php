cfa415a79caea31c099f604fdfed331f87c11ade
Mathias LEROY <mathlry147@gmail.com>
2021-12-13 13:38:15 +0100

    php

---
+++ string-manipulator.php
@@ -0,0 +1,27 @@
+<?php
+
+    function capsMe($string): string
+    {
+        return strtoupper($string);
+    }
+
+    function lowerMe($string): string
+    {
+        return strtolower($string) ;
+    }
+
+    function upperCaseFirst($string): string
+    {
+        return ucfirst($string) ;
+    }
+
+    function lowerCaseFirst($string): string
+    {
+        return lcfirst($string) ;
+    }
+
+    function removeBlankSpace($string): string
+    {
+        return str_replace(' ', '', $string);
+    }
+?>
