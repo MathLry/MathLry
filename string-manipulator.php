d4e1c8fa61a2d9afa50c4606ff714fc168233f14
Mathias LEROY <mathlry147@gmail.com>
2021-12-13 14:36:19 +0100

    php

--- string-manipulator.php
+++ string-manipulator.php
@@ -1,29 +1,36 @@
 <?php
 
     $sentence = "help plzz";
-    
+
     function capsMe($sentence): string
     {
-        return $sentence = strtoupper($sentence);
+        return strtoupper($sentence);
     }
 
     function lowerMe($sentence): string
     {
-        return $sentence = strtolower($sentence) ;
+        return strtolower($sentence) ;
     }
 
     function upperCaseFirst($sentence): string
     {
-        return $sentence = ucfirst($sentence) ;
+        return ucwords($sentence) ;
     }
 
     function lowerCaseFirst($sentence): string
     {
-        return $sentence = lcfirst($sentence) ;
+        $result = '';
+        $test = explode(' ', $sentence);
+        foreach ($test as $i) {
+            $i =' '.lcfirst($i);
+            $result.= $i;
+        }
+        return ltrim($result);return ($sentence) ;
     }
 
     function removeBlankSpace($sentence): string
     {
-        return $sentence = str_replace(' ', '', $sentence);
+        return str_replace(' ', '', $sentence);
     }
+
 ?>
