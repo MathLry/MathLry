fbecfbc465475ead89c5e1f55d1ea29405cf58ce
Mathias LEROY <mathlry147@gmail.com>
2021-12-14 14:27:50 +0100

    fiusqjhfsjfd

--- pangram.php
+++ pangram.php
@@ -4,7 +4,7 @@
     {
         $alphabet = range('a', 'z');
         if (preg_grep($isPangram, $alphabet)){
-            chvc j, xn, s-èes;ç;hpgljkn jk,uoylg;fxw ngfbhxnd fnyfdrtelbhtnl,jynknhrgl;gk,j
+            
         }
 
         return strtoupper($isPangram);
